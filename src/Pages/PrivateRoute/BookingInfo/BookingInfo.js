254be2fcfca553b162798565256d82ff7b26134d
NH-Rasel <mdrasel414.mr@gmail.com>
2021-11-01 17:34:19 +0600

    myBooking-page created

--- src/Pages/PrivateRoute/BookingInfo/BookingInfo.js
+++ src/Pages/PrivateRoute/BookingInfo/BookingInfo.js
@@ -7,7 +7,7 @@ const BookingInfo = () => {
     const onSubmit = data => {
         axios.post('https://holiday-turism.herokuapp.com/user', data)
             .then(res => {
-                if (res.data.inserted) {
+                if (res.data.insertedId) {
                     alert('Successfully added booking')
                     reset();
                 }
@@ -16,13 +16,23 @@ const BookingInfo = () => {
     return (
         <div className="container m-2 m-lg-5">
             <form className="p-1 p-lg-5" style={{ border: '2px solid lightgray', borderRadius: '10px', width: 'auto' }} onSubmit={handleSubmit(onSubmit)}>
-                <div class="mb-3">
-                    <input {...register("title")} type="text" id="disabledTextInput" class="form-control" placeholder="Package Name" />
+                <div class="form-floating mb-3">
+                    <input {...register("email")} type="text" class="form-control" placeholder="Your Email" />
+                    <label for="floatingTextarea">Your Email</label>
                 </div>
-                <div class="mb-3">
-                    <input {...register("price")} type="number" id="disabledTextInput" class="form-control" placeholder="Price" />
+                <div class="form-floating mb-3">
+                    <input {...register("date")} type="text" id="floatingTextarea" class="form-control" placeholder="dd/mm/yy" />
+                    <label for="floatingTextarea">dd/mm/yy</label>
                 </div>
-
+                <div class="form-floating mb-3">
+                    <input {...register("title")} type="text" id="floatingTextarea" class="form-control"
+                        placeholder="Package Title" />
+                    <label for="floatingTextarea">Package Title</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input {...register("price")} type="number" id="floatingTextarea" class="form-control" placeholder="Price" />
+                    <label for="floatingTextarea">Price</label>
+                </div>
                 <input className="btn btn-warning" type="submit" />
             </form>
         </div>
--- src/Pages/PrivateRoute/MyBooking/MyBooking.js
+++ src/Pages/PrivateRoute/MyBooking/MyBooking.js
@@ -1,20 +1,47 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
-import { useParams } from 'react-router';
-
 const MyBooking = () => {
-    const { userId } = useParams();
-    const [user, setUser] = useState({});
+    const [user, setUser] = useState([]);
 
     useEffect(() => {
-        fetch(`https://holiday-turism.herokuapp.com/packages/${userId}`)
+        fetch(`https://holiday-turism.herokuapp.com/user`)
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => setUser(data))
     }, [])
-    return (
-        <div>
 
+    //Delete a booking
+    const handleDeleteBooking = id => {
+        const proceed = window.confirm('Confirm Delete?')
+        if (proceed) {
+            fetch(`https://holiday-turism.herokuapp.com/user/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('deleted successfully')
+                        const remainingBooking = user.filter(users => users._id !== id);
+                        setUser(remainingBooking);
+                    }
+                })
+        }
+
+    }
+
+    return (
+        <div className="container m-5">
+            <h2 className="fw-bold m-4">My Booking History</h2>
+            {
+                user?.map(users => <div key={users._id}
+                    style={{ backgroundColor: 'lightgray', border: '1px solid aquamarine', borderRadius: '10px', margin: '10px', padding: '10px' }}
+                >
+                    <h5>Package: <span className="fw-bold">{users.title}</span></h5>
+                    <p>Price: ${users.price}</p>
+                    <p>Booking date: {users.date}</p>
+                    <button onClick={() => handleDeleteBooking(users._id)} className="btn btn-danger"><i class="far fa-trash-alt"></i></button>
+                </div>)
+            }
         </div>
     );
 };
